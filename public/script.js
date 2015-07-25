00845db5fc0643f37b8ef00c92a5542a340fe97a
Ricardo <ric.valechat@gmail.com>
2015-07-25 16:33:45 -0300

    Clean-up

--- public/script.js
+++ public/script.js
@@ -113,13 +113,6 @@ $(function() {
   });
   $("#header .search").focus();
 
-  var po = document.createElement('script');
-  po.type = 'text/javascript';
-  po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(po, s);
-
   var curAudio;
   var curAudioContainer;
 
